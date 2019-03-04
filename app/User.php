01cf10c1b4ccccc81ae0dc42439b63802fa139ac
twis4t <visi0n@mail.ru>
2019-03-04 23:15:00 +0300

    passport

--- app/User.php
+++ app/User.php
@@ -2,21 +2,32 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
-    /**
+    use HasApiTokens, Notifiable;
+    
+   /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'fio', 'email'
+    ];
+
+     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'role_id' => 'integer',
+        'shop_id' => 'integer',
     ];
 
     /**
@@ -25,15 +36,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password'
     ];
 }

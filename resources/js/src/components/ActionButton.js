6e302ca7a55854533a7881b25a82ce8d07e48ab2
twis4t <visi0n@mail.ru>
2019-03-19 23:20:33 +0300

    shops grid

---
+++ resources/js/src/components/ActionButton.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import IconButton from '@material-ui/core/IconButton'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+
+const ITEM_HEIGHT = 48
+
+const generateKey = pre => {
+  return `${pre}_${new Date().getTime()}`
+}
+
+class ActionButton extends React.Component {
+  state = {
+    anchorEl: null,
+  }
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget })
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null })
+  }
+
+  handleItemClick = callback => {
+    callback()
+    this.setState({ anchorEl: null })
+  }
+
+  render() {
+    const { anchorEl } = this.state
+    const { actions } = this.props
+    const open = Boolean(anchorEl)
+
+    return (
+      <div>
+        <IconButton
+          aria-label="More"
+          aria-owns={open ? 'long-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={this.handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: 200,
+            },
+          }}
+        >
+          {actions.map(option => (
+            <MenuItem key={generateKey(option.title)} onClick={() => this.handleItemClick(option.action)}>
+              {option.title}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
+    )
+  }
+}
+
+ActionButton.propTypes = {
+  actions: PropTypes.array.isRequired,
+}
+
+export default ActionButton
--- resources/js/src/containers/Shops/index.js
+++ resources/js/src/containers/Shops/index.js
@@ -6,17 +6,51 @@ import { withStyles } from '@material-ui/core/styles'
 import classNames from 'classnames'
 import styles from './styles'
 import rowData from './data'
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui'
-
+import ActionButton from '@/components/ActionButton'
+import { Grid as DxGrid, Table, TableHeaderRow, SearchPanel, Toolbar } from '@devexpress/dx-react-grid-material-ui'
+import { DataTypeProvider } from '@devexpress/dx-react-grid'
+import { SearchState, IntegratedFiltering } from '@devexpress/dx-react-grid'
 import { Paper } from '@material-ui/core'
+
+const ActionsList = [
+  {
+    title: 'Сотрудники',
+    action: () => {
+      console.log('users list')
+    },
+  },
+  {
+    title: 'Редактировать',
+    action: () => {
+      console.log('edit company')
+    },
+  },
+  {
+    title: 'Удалить',
+    action: () => {
+      console.log('remove company')
+    },
+  },
+]
+
+const ImageTypeProvider = props => <DataTypeProvider formatterComponent={ShopImage} {...props} />
+
+const ShopImage = ({ value }) => <img src={value} width={80} />
+
+ShopImage.propTypes = {
+  value: PropTypes.string.isRequired,
+}
+const ActionTypeProvider = props => <DataTypeProvider formatterComponent={ActionButtonFormatter} {...props} />
+
+const ActionButtonFormatter = props => <ActionButton actions={ActionsList} {...props} />
+
+ShopImage.propTypes = {
+  value: PropTypes.string.isRequired,
+}
 
 class Shops extends Component {
   state = {
     rowData: rowData,
-  }
-
-  makeShopLogo = logo => {
-    return <img src={logo} />
   }
 
   onGridReady = params => {
@@ -33,18 +67,29 @@ class Shops extends Component {
         <ModuleTitle title="Управление магазинами" />
 
         <Paper className={classNames(classes.paperCard, classes.flexGrow)}>
-          <Grid
+          <DxGrid
             rows={this.state.rowData}
             columns={[
-              { name: 'logo', title: 'logo' },
-              { name: 'name', title: 'name' },
-              { name: 'description', title: 'description' },
-              { name: 'staff', title: 'staff' },
+              { name: 'logo', title: 'Логотип', width: 100 },
+              { name: 'name', title: 'Наименование' },
+              { name: 'description', title: 'Описание' },
+              { name: 'address', title: 'Адрес' },
+              { name: 'phone', title: 'Телефон' },
+              { name: 'staff', title: 'Сотрудники' },
+              { name: 'id', title: 'Действия' },
             ]}
           >
-            <Table />
+            <ImageTypeProvider for={['logo']} />
+            <ActionTypeProvider for={['id']} />
+            <SearchState defaultValue="" />
+            <IntegratedFiltering />
+            <Table
+              columnExtensions={[{ columnName: 'logo', width: 130 }, { columnName: 'id', width: 100, align: 'center' }]}
+            />
+            <Toolbar />
+            <SearchPanel />
             <TableHeaderRow />
-          </Grid>
+          </DxGrid>
         </Paper>
       </div>
     )

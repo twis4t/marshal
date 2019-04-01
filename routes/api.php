8bb3ebb84245f542d31e9c1132abd9b6a0a1013f
twis4t <visi0n@mail.ru>
2019-04-01 23:38:06 +0300

    api routes

--- routes/api.php
+++ routes/api.php
@@ -28,4 +28,18 @@ Route::group(['middleware' => 'auth:api'], function(){
 	Route::put('category-update/{id}', 'API\CategoryController@update');
 	Route::delete('category-detele/{id}', 'API\CategoryController@destroy');
 
+	/* Request */
+	Route::get('requests', 'API\RequestController@index');
+	Route::get('request/{id}', 'API\RequestController@show');
+	Route::post('request-add', 'API\RequestController@store');
+	Route::put('request-update/{id}', 'API\RequestController@update');
+	Route::delete('request-detele/{id}', 'API\RequestController@destroy');
+
+	/* Answer */
+	Route::get('answers', 'API\AnswerController@index');
+	Route::get('answer/{id}', 'API\AnswerController@show');
+	Route::post('answer-add', 'API\AnswerController@store');
+	Route::put('answer-update/{id}', 'API\AnswerController@update');
+	Route::delete('answer-detele/{id}', 'API\AnswerController@destroy');
+
 });

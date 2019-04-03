20ab8d5b506b73f20708765b3b1d385737e22a4d
twis4t <visi0n@mail.ru>
2019-04-03 23:38:20 +0300

    cat upd

--- resources/js/src/components/Shops/CategoryModal.js
+++ resources/js/src/components/Shops/CategoryModal.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 import { withStyles } from '@material-ui/core/styles'
 import {
+  Avatar,
   Button,
   Dialog,
   DialogActions,
@@ -13,55 +15,83 @@ import {
   ListItemText,
   IconButton,
   Tooltip,
+  TextField,
 } from '@material-ui/core'
-import { LinkOff as LinkOffIcon, Lock as LockIcon } from '@material-ui/icons'
+import { Clear as ClearIcon } from '@material-ui/icons'
 
 const styles = theme => ({
   root: {
     backgroundColor: theme.palette.background.paper,
     width: 500,
   },
+  activeCategory: {
+    backgroundColor: '#8bc34a',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
 })
 
 class CategoryModal extends React.Component {
   state = {
     value: 0,
+    currentCategory: {
+      id: 0,
+      name: '',
+    },
   }
 
-  handleChange = (event, value) => {
-    this.setState({ value })
-  }
-
-  handleChangeIndex = index => {
-    this.setState({ value: index })
+  handleChangeName = event => {
+    this.setState({
+      currentCategory: {
+        id: this.state.currentCategory.id,
+        name: event.target.value,
+      },
+    })
   }
 
   render() {
-    const { status, onClose } = this.props
+    const { status, onClose, classes } = this.props
     return (
       <div>
         <Dialog
           open={status}
           onClose={onClose}
+          fullWidth={true}
           aria-labelledby="alert-dialog-title"
           aria-describedby="alert-dialog-description"
         >
           <DialogTitle id="alert-dialog-title">Категории</DialogTitle>
           <DialogContent>
+            <form className={classes.container} noValidate autoComplete="off">
+              <TextField
+                id="outlined-name"
+                label="Категория"
+                className={classes.textField}
+                value={this.state.currentCategory.name}
+                onChange={this.handleChangeName}
+                margin="normal"
+                variant="outlined"
+              />
+            </form>
             <List>
               {this.props.data.map(value => (
                 <ListItem key={value.category} button>
-                  <ListItemText primary={value.category} secondary={'test@test.ru'} />
+                  <Avatar className={value.shops.length > 0 ? classes.activeCategory : null}>
+                    {value.shops.length || 0}
+                  </Avatar>
+                  <ListItemText
+                    primary={value.category}
+                    secondary={'Добавлена: ' + moment(value.created_at).format('DD.MM.YYYY HH:mm:SS')}
+                  />
                   <ListItemSecondaryAction>
-                    <Tooltip title="Заблокировать">
-                      <IconButton aria-label="Ban">
-                        <LockIcon fontSize="small" />
-                      </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Исключить">
-                      <IconButton aria-label="Delete">
-                        <LinkOffIcon fontSize="small" />
-                      </IconButton>
+                    <Tooltip title="Удалить">
+                      <div>
+                        <IconButton aria-label="Ban" disabled={value.shops.length > 0}>
+                          <ClearIcon fontSize="small" />
+                        </IconButton>
+                      </div>
                     </Tooltip>
                   </ListItemSecondaryAction>
                 </ListItem>

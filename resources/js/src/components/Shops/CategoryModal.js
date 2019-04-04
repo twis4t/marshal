50d07059d9ba0619cbfc7652305fb1ba404b5235
twis4t <visi0n@mail.ru>
2019-04-04 23:15:57 +0300

    cat modal

--- resources/js/src/components/Shops/CategoryModal.js
+++ resources/js/src/components/Shops/CategoryModal.js
@@ -16,8 +16,11 @@ import {
   IconButton,
   Tooltip,
   TextField,
+  Menu,
+  Paper,
+  InputAdornment,
 } from '@material-ui/core'
-import { Clear as ClearIcon } from '@material-ui/icons'
+import { Clear as ClearIcon, AddBox as AddBoxIcon } from '@material-ui/icons'
 
 const styles = theme => ({
   root: {
@@ -28,25 +31,59 @@ const styles = theme => ({
     backgroundColor: '#8bc34a',
   },
   textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
+    /*marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,*/
+    paddingRight: 4,
+  },
+  editForm: {
+    outline: 'none',
+    display: 'flex',
+    flexDirection: 'row',
+    boxShadow: 'none',
+    paddingLeft: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+  },
+  editField: {
+    marginRight: 10,
+  },
+  menuWrapper: {
+    /*paddintTop: 0,
+    paddintBottom: 0,*/
+  },
+  noShadow: {
+    boxShadow: 'none',
   },
 })
 
 class CategoryModal extends React.Component {
   state = {
-    value: 0,
-    currentCategory: {
+    menuAnchorEl: null,
+    newCategory: '',
+    editableCategory: {
       id: 0,
-      name: '',
+      category: '',
     },
   }
 
-  handleChangeName = event => {
+  menuHandleClick = (event, val) => {
+    this.setState({ menuAnchorEl: event.currentTarget, editableCategory: val })
+  }
+
+  menuHandleClose = () => {
+    this.setState({ menuAnchorEl: null })
+  }
+
+  handleChangeNewName = event => {
     this.setState({
-      currentCategory: {
-        id: this.state.currentCategory.id,
-        name: event.target.value,
+      newCategory: event.target.value,
+    })
+  }
+
+  handleEditName = event => {
+    this.setState({
+      editableCategory: {
+        ...this.state.editableCategory,
+        category: event.target.value,
       },
     })
   }
@@ -64,20 +101,29 @@ class CategoryModal extends React.Component {
         >
           <DialogTitle id="alert-dialog-title">Категории</DialogTitle>
           <DialogContent>
-            <form className={classes.container} noValidate autoComplete="off">
-              <TextField
-                id="outlined-name"
-                label="Категория"
-                className={classes.textField}
-                value={this.state.currentCategory.name}
-                onChange={this.handleChangeName}
-                margin="normal"
-                variant="outlined"
-              />
-            </form>
+            <TextField
+              id="outlined-name"
+              placeholder="Добавить категорию"
+              className={classes.textField}
+              value={this.state.newCategory}
+              onChange={this.handleChangeNewName}
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton aria-label="Toggle password visibility" onClick={''}>
+                      <AddBoxIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+
             <List>
               {this.props.data.map(value => (
-                <ListItem key={value.category} button>
+                <ListItem key={value.category} button onClick={e => this.menuHandleClick(e, value)}>
                   <Avatar className={value.shops.length > 0 ? classes.activeCategory : null}>
                     {value.shops.length || 0}
                   </Avatar>
@@ -97,10 +143,31 @@ class CategoryModal extends React.Component {
                 </ListItem>
               ))}
             </List>
+            <Menu
+              className={classes.menuWrapper}
+              classes={{ paper: classes.noShadow }}
+              id="simple-menu"
+              anchorEl={this.state.menuAnchorEl}
+              open={Boolean(this.state.menuAnchorEl)}
+              onClose={this.menuHandleClose}
+            >
+              <Paper className={classes.editForm}>
+                <TextField
+                  className={classes.editField}
+                  id="outlined-name"
+                  value={this.state.editableCategory.category}
+                  variant="outlined"
+                  onChange={this.handleEditName}
+                />
+                <Button variant="outlined" color="primary">
+                  Сохранить
+                </Button>
+              </Paper>
+            </Menu>
           </DialogContent>
           <DialogActions>
             <Button onClick={onClose} color="primary" autoFocus>
-              OK
+              Закрыть
             </Button>
           </DialogActions>
         </Dialog>

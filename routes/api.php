97124de46b05fa5c7d7bb36e839320792a2eec5c
twis4t <visi0n@mail.ru>
2019-04-23 21:55:06 +0300

    delete

--- routes/api.php
+++ routes/api.php
@@ -24,14 +24,14 @@ Route::group(['middleware' => 'auth:api'], function(){
 	Route::post('shop-add', 'API\ShopController@store');
 	Route::put('shop-update/{id}', 'API\ShopController@update');
 	Route::put('shop-set-categories/{id}', 'API\ShopController@setCategories');
-	Route::delete('shop-detele/{id}', 'API\ShopController@destroy');
+	Route::delete('shop-delete/{id}', 'API\ShopController@destroy');
 	
 	/* Category */
 	Route::get('categories', 'API\CategoryController@index');
 	Route::get('category/{id}', 'API\CategoryController@show');
 	Route::post('category-add', 'API\CategoryController@store');
 	Route::put('category-update/{id}', 'API\CategoryController@update');
-	Route::delete('category-detele/{id}', 'API\CategoryController@destroy');
+	Route::delete('category-delete/{id}', 'API\CategoryController@destroy');
 
 	/* Request */
 	Route::get('requests', 'API\RequestController@index');
@@ -39,14 +39,14 @@ Route::group(['middleware' => 'auth:api'], function(){
 	Route::get('request/{id}', 'API\RequestController@show');
 	Route::post('request-add', 'API\RequestController@store');
 	Route::put('request-update/{id}', 'API\RequestController@update');
-	Route::delete('request-detele/{id}', 'API\RequestController@destroy');
+	Route::delete('request-delete/{id}', 'API\RequestController@destroy');
 
 	/* Answer */
 	Route::get('answers', 'API\AnswerController@index');
 	Route::get('answer/{id}', 'API\AnswerController@show');
 	Route::post('answer-add', 'API\AnswerController@store');
 	Route::put('answer-update/{id}', 'API\AnswerController@update');
-	Route::delete('answer-detele/{id}', 'API\AnswerController@destroy');
+	Route::delete('answer-delete/{id}', 'API\AnswerController@destroy');
 
 	/* Messages */
 	Route::get('messages', 'API\MessageController@index');
@@ -54,21 +54,21 @@ Route::group(['middleware' => 'auth:api'], function(){
 	Route::get('message/{id}', 'API\MessageController@show');
 	Route::post('message-add', 'API\MessageController@store');
 	Route::put('message-update/{id}', 'API\MessageController@update');
-	Route::delete('message-detele/{id}', 'API\MessageController@destroy');
+	Route::delete('message-delete/{id}', 'API\MessageController@destroy');
 	
 	/* Car */
 	Route::get('cars', 'API\CarController@index');
 	Route::get('car/{id}', 'API\CarController@show');
 	Route::post('car-add', 'API\CarController@store');
 	Route::put('car-update/{id}', 'API\CarController@update');
-	Route::delete('car-detele/{id}', 'API\CarController@destroy');
+	Route::delete('car-delete/{id}', 'API\CarController@destroy');
 
 	/* CarBrands */
 	Route::get('carbrands', 'API\CarBrandController@index');
 	Route::get('carbrand/{id}', 'API\CarBrandController@show');
 	Route::post('carbrand-add', 'API\CarBrandController@store');
 	Route::put('carbrand-update/{id}', 'API\CarBrandController@update');
-	Route::delete('carbrand-detele/{id}', 'API\CarBrandController@destroy');
+	Route::delete('carbrand-delete/{id}', 'API\CarBrandController@destroy');
 
 	/* CarModels */
 	Route::get('carmodels', 'API\CarModelController@index');
@@ -76,6 +76,6 @@ Route::group(['middleware' => 'auth:api'], function(){
 	Route::get('carmodel/{id}', 'API\CarModelController@show');
 	Route::post('carmodel-add', 'API\CarModelController@store');
 	Route::put('carmodel-update/{id}', 'API\CarModelController@update');
-	Route::delete('carmodel-detele/{id}', 'API\CarModelController@destroy');
+	Route::delete('carmodel-delete/{id}', 'API\CarModelController@destroy');
 
 });

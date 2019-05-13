12074235271ca0de88b22e3c73a4dbc9ca1f2357
twis4t <visi0n@mail.ru>
2019-05-13 23:11:44 +0300

    reset password

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,16 @@ use Illuminate\Http\Request;
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
+Route::group([    
+	'namespace' => 'Auth',    
+	'middleware' => 'api',    
+	'prefix' => 'password'
+], function () {    
+	Route::post('create', 'PasswordResetController@create');
+	Route::get('find/{token}', 'PasswordResetController@find');
+	Route::post('reset', 'PasswordResetController@reset');
+});
+
 Route::group(['middleware' => 'auth:api'], function(){
 	Route::get('details', 'API\UserController@details');
 

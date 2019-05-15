e82f1b249b65b7cfac57ad4f2bdf19ef4a494b1f
twis4t <visi0n@mail.ru>
2019-05-15 20:33:56 +0300

    fix files

--- app/Http/Controllers/API/BannerController.php
+++ app/Http/Controllers/API/BannerController.php
@@ -20,7 +20,14 @@ class BannerController extends Controller
      */
     public function index()
     {
-        return Banner::all();
+        $banners = Banner::all();
+        foreach ($banners as $key => $banner){            
+            if ($banner->banner != null){                
+                $banner->banner = "data:image/x-icon;base64," . base64_encode(Storage::get($banner->banner));
+                $banners[$key] = $banner;
+            }            
+        }
+        return $banners;
     }
 
     /**
@@ -36,6 +43,27 @@ class BannerController extends Controller
     public function store(Request $request)
     {
         $requestData = $request->all();
+        $requestData['banner'] = '';
+
+        // Если пришел файл - обрабатываем
+        if ($request->hasFile('banner')){
+            $path = $request->file('banner')->store('banners');
+            $requestData['banner'] = $path;
+        } elseif (isset($request->banner)){
+            // Если пришла строка, то base64?
+            if (preg_match('/^data:image\/(.*?);base64,/', $request->banner)) {
+                // Указано ли имя?
+                if (preg_match('/name=(.*?)\.(\w*?);/', $request->banner, $matches)) {
+                    $fileName = $matches[1];
+                    $fileExt = $matches[2];
+                    $encodedImg = substr($request->banner, strpos($request->banner, ',') + 1);            
+                    $img = base64_decode($encodedImg);   
+                    $path = 'banners/'.md5(time().$fileName).".".$fileExt;     
+                    Storage::put($path, $img);
+                    $requestData['banner'] = $path;
+                }
+            }
+        }
         $result = Banner::create($requestData);
         return response()->json(['result' => $result], 200);
     }
@@ -49,6 +77,9 @@ class BannerController extends Controller
     public function show($id)
     {
         $banner = Banner::find($id);
+        if (isset($banner->banner) && $banner->banner != null){
+            $banner->banner = "data:image/x-icon;base64," . base64_encode(Storage::get($banner->banner));
+        }  
         if ($banner == null) return response()->json(['error'=>'Banner not found'], 404);
         return $banner;
     }
@@ -67,6 +98,26 @@ class BannerController extends Controller
     public function update(Request $request, $id)
     {
         $requestData = $request->all();
+        $requestData['banner'] = '';
+         // Если пришел файл - обрабатываем
+        if ($request->hasFile('banner')){
+            $path = $request->file('banner')->store('banners');
+            $requestData['banner'] = $path;
+        } elseif (isset($request->banner)){
+            // Если пришла строка, то base64?
+            if (preg_match('/^data:image\/(.*?);base64,/', $request->banner)) {
+                // Указано ли имя?
+                if (preg_match('/name=(.*?)\.(\w*?);/', $request->banner, $matches)) {
+                    $fileName = $matches[1];
+                    $fileExt = $matches[2];
+                    $encodedImg = substr($request->banner, strpos($request->banner, ',') + 1);            
+                    $img = base64_decode($encodedImg);   
+                    $path = 'banners/'.md5(time().$fileName).".".$fileExt;     
+                    Storage::put($path, $img);
+                    $requestData['banner'] = $path;
+                }
+            }
+        }
         $result = Banner::where('id', $id)->update($requestData);
         return response()->json(['result' => $result], 200);
     }
